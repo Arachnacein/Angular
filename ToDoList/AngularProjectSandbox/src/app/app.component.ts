a7d01fb06bc782344f2dd1226d79b1a395700043
Jakub <jakub26800@gmail.com>
2025-02-01 17:09:25 +0100

    Added users list.

--- ToDoList/AngularProjectSandbox/src/app/app.component.ts
+++ ToDoList/AngularProjectSandbox/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header.component';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true,
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, UserComponent]
 })
 export class AppComponent {
   title = 'AngularProjectSandbox';
