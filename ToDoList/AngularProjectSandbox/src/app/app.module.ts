4726cf61f5aebb45d0c25aba8c272ba0554de604
Jakub <jakub26800@gmail.com>
2025-02-11 22:48:53 +0100

    Added dynamic binding tasks list.

--- ToDoList/AngularProjectSandbox/src/app/app.module.ts
+++ ToDoList/AngularProjectSandbox/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { HeaderComponent } from './header/header.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { TaskComponent } from './tasks/task/task.component';
 
 @NgModule({
   declarations: [
     /*AppComponent*/
   
-    TasksComponent
+    TasksComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
