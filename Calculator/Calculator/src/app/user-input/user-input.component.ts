a601ecadf0601931109ea5f05f15fa7be8e2920a
Jakub <jakub26800@gmail.com>
2025-05-14 23:07:18 +0200

    Adde two-way form binding.

--- Calculator/Calculator/src/app/user-input/user-input.component.ts
+++ Calculator/Calculator/src/app/user-input/user-input.component.ts
@@ -1,11 +1,24 @@
 import { Component, Input } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [FormsModule],
   standalone: true,
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+
+  onSubmit() {
+    console.log('SUBMITTED');
+    console.log(this.enteredAnnualInvestment);
+    console.log(this.enteredDuration);
+    console.log('SUBMITTED');
+  }
 }
