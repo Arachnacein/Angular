5c672d72223d6e98f1eff65bc8dc6e325734a104
Jakub <jakub26800@gmail.com>
2025-05-18 23:29:26 +0200

    Added signals

--- Calculator/Calculator/src/app/investment-results/investment-results.component.ts
+++ Calculator/Calculator/src/app/investment-results/investment-results.component.ts
@@ -1,18 +1,19 @@
-import { Component, Input } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-investment-results',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
 export class InvestmentResultsComponent {
-  @Input() results?: {
+  results = input<{
     year: number;
     interest: number;
     valueEndOfYear: number;
     annualInvestment: number;
     totalInterest: number;
     totalAmountInvested: number;
-  }[];
+  }[]>();
 }
