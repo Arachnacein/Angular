6c756b1bb82a40199062cc0a312421a7bed1a8ba
Jakub <jakub26800@gmail.com>
2025-05-23 19:02:39 +0200

    Added InvestmentService

--- Calculator/Calculator/src/app/user-input/user-input.component.ts
+++ Calculator/Calculator/src/app/user-input/user-input.component.ts
@@ -1,6 +1,6 @@
-import { Component, output, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
-import { InvestmentInput } from '../investment-input.model';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-user-input',
@@ -10,20 +10,21 @@ import { InvestmentInput } from '../investment-input.model';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-  calculate = output<InvestmentInput>();
   enteredInitialInvestment = signal('0');
   enteredAnnualInvestment = signal ('0');
   enteredExpectedReturn = signal('5');
   enteredDuration = signal('10');
 
+  private investmentService = inject(InvestmentService);
 
   onSubmit() {
-    this.calculate.emit({
+    this.investmentService.calculateInvestmentResults({
       initialInvestment: +this.enteredInitialInvestment(),
       duration: +this.enteredDuration(),
       expectedReturn: +this.enteredExpectedReturn(),
       annualInvestment: +this.enteredAnnualInvestment()
     });
+
     this.enteredAnnualInvestment.set('0');
     this.enteredInitialInvestment.set('0');
     this.enteredExpectedReturn.set('5');
