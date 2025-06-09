f7110716110d5ea7e75d2a35cf742431eacf333f
Jakub <jakub26800@gmail.com>
2025-06-09 14:35:37 +0200

    Make components reusable. Wrapping, content projection

--- ManagementApp/ManagementApp/src/app/app.component.ts
+++ ManagementApp/ManagementApp/src/app/app.component.ts
@@ -3,11 +3,17 @@ import { HeaderComponent } from './header/header.component';
 import { TrafiicComponent } from './dashboard/trafiic/trafiic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
 import { ServerStatusComponent } from './dashboard/server-status/server-status.component';
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, TrafiicComponent, TicketsComponent, ServerStatusComponent],
+  imports: [
+    HeaderComponent, 
+    TrafiicComponent, 
+    TicketsComponent, 
+    ServerStatusComponent, 
+    DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
