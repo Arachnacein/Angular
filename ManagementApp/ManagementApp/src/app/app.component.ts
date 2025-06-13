11675949cfcb4851009c8265ce1b0d33337564c4
Jakub <jakub26800@gmail.com>
2025-06-13 13:10:14 +0200

    Working with host

--- ManagementApp/ManagementApp/src/app/app.component.ts
+++ ManagementApp/ManagementApp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, inject } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { TrafiicComponent } from './dashboard/trafiic/trafiic.component';
 import { TicketsComponent } from './dashboard/tickets/tickets.component';
@@ -14,10 +14,19 @@ import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-ite
     TicketsComponent, 
     ServerStatusComponent, 
     DashboardItemComponent],
+    host: {
+      class: 'app-root',
+      '(click)': 'onClick()'
+    },
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   title = 'Chart App';
+  private element = inject(ElementRef);
 
-
+  onClick()
+  {
+    console.log('AppComponent clicked');
+    console.log(this.element);
+  }
 }
